768ea8c54b7ef2e4e5f108ff9ac6918d17a507a3
Maxim Kaschenko <kaschenkomaksim@gmail.com>
2010-05-11 18:18:41 +0700

    Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 ActionController::Routing::Routes.draw do |map|
+  map.resources :fireworks, :only => [:index, :new, :show], :as => 'gallery'
+
   map.resources :invitations, :only => [:index, :new, :create]
 end
