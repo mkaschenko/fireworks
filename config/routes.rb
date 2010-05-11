23a9c7b4f0013bbbbc3895c26836e6a92f64ed56
Maxim Kaschenko <kaschenkomaksim@gmail.com>
2010-05-11 18:31:24 +0700

    Gallery to root url

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ ActionController::Routing::Routes.draw do |map|
   map.resources :fireworks, :only => [:index, :new, :show], :as => 'gallery'
 
   map.resources :invitations, :only => [:index, :new, :create]
+
+  map.root :controller => 'fireworks'
 end
