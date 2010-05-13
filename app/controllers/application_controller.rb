e9ab2866de808b927559aecdd0dcdf3ac68c45b2
Maxim Kaschenko <kaschenkomaksim@gmail.com>
2010-05-13 20:37:30 +0700

    Migrated to iframe

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,20 +1,29 @@
 class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
-  protect_from_forgery # See ActionController::RequestForgeryProtection for details
 
-  # Scrub sensitive parameters from your log
-  # filter_parameter_logging :password
+  attr_reader :current_user, :facebooker_config
+  helper_attr :current_user, :facebooker_config, :fb_url
 
   ensure_authenticated_to_facebook
+  # ensure_application_is_installed_by_facebook_user
 
-  attr_reader :current_user
-  helper_attr :current_user
+  before_filter :set_current_user, :set_facebooker_config
 
-  before_filter :set_current_user
+  protected
+
+  def fb_url(relative_path = "")
+    apps_url = 'http://apps.facebook.com'
+    canvas_page_name = facebooker_config['canvas_page_name']
+    File.join(apps_url, canvas_page_name, relative_path)
+  end
 
   private
 
   def set_current_user
     @current_user = User.find_or_create_by_facebook_id(facebook_session.user.uid)
+  end
+
+  def set_facebooker_config
+    @facebooker_config = Facebooker::AdapterBase.facebooker_config
   end
 end
--- config/environment.rb
+++ config/environment.rb
@@ -3,8 +3,6 @@ RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
-  config.action_controller.allow_forgery_protection = false
-
   config.gem "haml", :version => ">= 2.2.22"
 
   config.time_zone = 'UTC'
