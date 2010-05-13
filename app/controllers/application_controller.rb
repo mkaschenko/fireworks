1b0288d1bdcfc23aa17737f5033c46653f52edd7
Maxim Kaschenko <kaschenkomaksim@gmail.com>
2010-05-13 21:32:34 +0700

    Some fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,19 +1,19 @@
 class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
 
-  attr_reader :current_user, :facebooker_config
-  helper_attr :current_user, :facebooker_config, :fb_url
+  attr_reader :current_user
+  helper_attr :current_user, :fb_url
 
   ensure_authenticated_to_facebook
   # ensure_application_is_installed_by_facebook_user
 
-  before_filter :set_current_user, :set_facebooker_config
+  before_filter :set_current_user
 
   protected
 
   def fb_url(relative_path = "")
     apps_url = 'http://apps.facebook.com'
-    canvas_page_name = facebooker_config['canvas_page_name']
+    canvas_page_name = 'sn_fireworks'
     File.join(apps_url, canvas_page_name, relative_path)
   end
 
@@ -21,9 +21,5 @@ class ApplicationController < ActionController::Base
 
   def set_current_user
     @current_user = User.find_or_create_by_facebook_id(facebook_session.user.uid)
-  end
-
-  def set_facebooker_config
-    @facebooker_config = Facebooker::AdapterBase.facebooker_config
   end
 end
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,13 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+$(document).ready(function() {
+
+  FB_RequireFeatures(["XFBML"], function() {
+    FB.Facebook.init("ee04c13f7e3ce1a4a602f1cc10835b52", "/xd_receiver.htm");
+  });
+
+  FB_RequireFeatures(["CanvasUtil"], function() {
+    FB.XdComm.Server.init("/xd_receiver.htm");
+    FB.CanvasClient.startTimerToSizeToContent();
+    FB.CanvasClient.syncUrl();
+  });
+
+});
